b434ad2bfe1f9147434cf556d7e09fcc2e8a4932
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-10 17:10:35 +0900

    define type Simulator

--- src/types.nim
+++ src/types.nim
@@ -1,6 +1,13 @@
 import intbrg
+import tables
+import sets
 
 type Agent* = ref object
   id*: int
   belief*: Formulae
   opinion*: range[0.0..1.0]
+
+type Simulator* = ref object
+  graph*: Table[int, HashSet[int]]
+  agents*: seq[Agent]
+  topic*: Formulae
