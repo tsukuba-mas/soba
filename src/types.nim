f23a4cee8f2344180f0b442a32f7ec7eef6555ab
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-13 14:33:34 +0900

    define type Message

--- src/types.nim
+++ src/types.nim
@@ -1,11 +1,20 @@
 import intbrg
 import tables
 import sets
+import options
 
 type Agent* = ref object
   id*: int
   belief*: Formulae
   opinion*: range[0.0..1.0]
+
+type Message* = ref object
+  author*: int
+  belief*: Formulae
+  opinion*: range[0.0..1.0]
+  postedAt*: int
+  repostedBy*: Option[int]
+  repostedAt*: Option[int]
 
 type Simulator* = ref object
   graph*: Table[int, HashSet[int]]
