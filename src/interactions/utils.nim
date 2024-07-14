a401678a1cfd6d43f153b047d574fe960a61102b
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-14 20:00:54 +0900

    filter posts following to agent's filter strategy

--- src/interactions/utils.nim
+++ src/interactions/utils.nim
@@ -1,7 +1,10 @@
 import ../types
+import ../distance
 import sequtils
 import sets
 import options
+
+const eps = 1e-5
 
 proc isRelatedToNeighbors(neighbors: HashSet[int], post: Message): bool =
   let isInitialAuthor = neighbors.contains(post.author)
@@ -9,4 +12,15 @@ proc isRelatedToNeighbors(neighbors: HashSet[int], post: Message): bool =
   isInitialAuthor or isRepostAuthor
 
 proc postSelector*(agent: Agent, posts: seq[Message]): seq[Message] =
-  posts.filterIt(agent.neighbors.isRelatedToNeighbors(it))
+  let availablePosts = posts.filterIt(agent.neighbors.isRelatedToNeighbors(it))
+  result = case agent.filterStrategy
+    of FilterStrategy.all:
+      availablePosts
+    of FilterStrategy.obounded:
+      availablePosts.filterIt(distance(agent.opinion, it.opinion) <= eps)
+    of FilterStrategy.bbounded:
+      availablePosts.filterIt(distance(agent.belief, it.belief) <= 1)
+    of FilterStrategy.both:
+      availablePosts.filterIt(
+        distance(agent.opinion, it.opinion) <= eps and distance(agent.belief, it.belief) <= 1
+      )
