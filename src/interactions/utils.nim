991cd4ff1ab5a127c114baa07d9662dd80da77a7
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-14 19:43:18 +0900

    refactor: move the procedure to check whether the post can be considered or not to another procedure

--- src/interactions/utils.nim
+++ src/interactions/utils.nim
@@ -3,7 +3,10 @@ import sequtils
 import sets
 import options
 
+proc isRelatedToNeighbors(neighbors: HashSet[int], post: Message): bool =
+  let isInitialAuthor = neighbors.contains(post.author)
+  let isRepostAuthor = post.repostedBy.isSome() and neighbors.contains(post.repostedBy.get())
+  isInitialAuthor or isRepostAuthor
+
 proc postSelector*(agent: Agent, posts: seq[Message]): seq[Message] =
-  posts.filterIt(
-    agent.neighbors.contains(it.author) or (it.repostedBy.isSome() and agent.neighbors.contains(it.repostedBy.get()))
-  )
+  posts.filterIt(agent.neighbors.isRelatedToNeighbors(it))
