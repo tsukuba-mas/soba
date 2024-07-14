99dd510d0b088b78cd41dd21f79c86ad7d64b55a
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-14 20:31:03 +0900

    do nothing if there are no other beliefs in brg

--- src/interactions/brg.nim
+++ src/interactions/brg.nim
@@ -6,6 +6,8 @@ import utils
 
 proc beliefRevisionGames(simulator: Simulator, agent: Agent): Agent =
   let neighborBeliefs = agent.postSelector(simulator.posts).mapIt(it.belief)
+  if neighborBeliefs.len == 0:
+    return agent
   let updatedBelief = r3(agent.belief, neighborBeliefs, hamming, sum)
   agent.updateBelief(updatedBelief)
 
