20676680fc3f058bf4143c418bf9e99e125c5bbc
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-17 15:38:05 +0900

    save graph structures

--- src/logger.nim
+++ src/logger.nim
@@ -3,6 +3,7 @@ import types
 import sequtils
 import intbrg
 import os
+import sets
 
 let beliefHist = "belhist.csv"
 let ophist = "ophist.csv"
@@ -22,14 +23,22 @@ proc initLogger*(outputTo: string) =
     removeDir(dirname)
   createDir(dirname)
 
-proc appendToFile(path: string, content: string) =
+proc appendToFile(path: string, content: string, tick: int) =
   var f = open(path, fmAppend)
   defer:
     f.close()
-  f.write(content & "\n")
+  f.write($tick & "," & content & "\n")
 
-proc log*(simulator: Simulator) =
+proc graphLogger(simulator: Simulator, tick: int) =
+  let saveTo = dirname & "/" & "graph.csv"
+  for agent in simulator.agents:
+    for next in agent.neighbors:
+      saveTo.appendToFile($agent.id & "," & $next, tick)
+
+proc log*(simulator: Simulator, tick: int) =
   let beliefs = simulator.agents.mapIt($(it.belief)).join(",")
   let opinions = simulator.agents.mapIt($(it.opinion)).join(",")
-  getBeliefHistPath().appendToFile(beliefs)
-  getOpinionHistPath().appendToFile(opinions)
+  getBeliefHistPath().appendToFile(beliefs, tick)
+  getOpinionHistPath().appendToFile(opinions, tick)
+  if tick mod 1000 == 0:
+    simulator.graphLogger(tick)
