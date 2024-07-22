1dc07eb95b17fc04f059e9b3b9d998e927225258
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-23 06:52:14 +0900

    switch clustering algorithm

--- visualization/graph.py
+++ visualization/graph.py
@@ -4,36 +4,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import util
+import clustering
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dir")
 parser.add_argument("-t", "--tick", action='append', type=int)
-parser.add_argument("-e", "--eps", type=float, default=0.05)
+parser.add_argument("-e", "--eps", type=float, default=0.1)
 args = parser.parse_args()
 
 DIR = args.dir
 OPHIST = util.readOphist(DIR)
 GRAPHS = util.readGraph(DIR)
 
-AGENTS = len(OPHIST[0]) - 1
+AGENTS = len(OPHIST[0])
 EPS = args.eps
-
-def classify():
-    group = {}
-    LAST_OPS = OPHIST[-1]
-    for i, op in enumerate(LAST_OPS):
-        notyet = True
-        for g in group:
-            if notyet and abs(g - op) <= 2 * EPS:
-                group[g].append(i)
-                notyet = False
-        if notyet:
-            group[op] = [i]
-    
-    centers = list(group.keys())
-    centers.sort()
-    v2k = { v: k for k in group for v in group[k] }
-    v2g = {v: centers.index(v2k[v]) for v in v2k}
-    return v2g
 
 def mValue(tick: int) -> float:
     ops = [o for o in OPHIST[tick]]
@@ -44,21 +27,22 @@ def mValue(tick: int) -> float:
         m += abs(bins[i] - bins[i-1])
     return m / maxCount
 
-def draw(tick: int):
+def draw(tick: int, clusterTick: int):
     G = nx.DiGraph()
     plt.figure(figsize=(15, 15))
-    group = classify()
-    G.add_nodes_from([
-        (i, {
-            "color": f"#{int((1.0 - OPHIST[tick][i+1]) * 255):02x}00{int(OPHIST[tick][i+1] * 255):02x}",
-            "subset": group[i],
-            })
-        for i in range(AGENTS)
-    ])
+    group = clustering.getClusterId(OPHIST[clusterTick], EPS)
+    colors = [int(c) for c in np.linspace(0, 255, len(set(group.values())))]
+    for a in range(AGENTS):
+        clusterId = group[a]
+        G.add_node(
+            a, 
+            color=f"#{colors[clusterId]:02x}00{255 - colors[clusterId]:02x}",
+            subset=clusterId
+        )
     EDGES = [(u, v) for (t, u, v) in GRAPHS if t == tick]
    
     for (u, v) in EDGES:
-        G.add_edge(u, v, weight=abs(OPHIST[tick][u+1] - OPHIST[tick][v+1]))
+        G.add_edge(u, v, weight=abs(OPHIST[tick][u] - OPHIST[tick][v]))
     nx.draw_networkx(G, 
                      pos=nx.multipartite_layout(G), 
                      node_color=[v["color"] for v in G.nodes.values()])
@@ -66,4 +50,5 @@ def draw(tick: int):
     plt.savefig(f"graph-{tick}.png")
 
 for tick in args.tick:
-    draw(tick)
+    draw(tick, max(args.tick))
+    print(tick)
