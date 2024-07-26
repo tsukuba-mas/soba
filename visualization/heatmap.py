bdf2d7a174c8fdf9e64fece6909dfef53705392f
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-26 10:07:50 +0900

    set vmin and vmax

--- visualization/heatmap.py
+++ visualization/heatmap.py
@@ -28,7 +28,7 @@ def saveHeatmap(agent2cluster: dict[int, int], filepath: str):
         vid = f"to-{agent2cluster[v]}"
         d[uid][vid] += 1
     df = pd.DataFrame(d)
-    sns.heatmap(df, annot=True, square=True, fmt='d')
+    sns.heatmap(df, annot=True, square=True, fmt='d', vmin=0, vmax=len(graphAtTick))
     plt.savefig(filepath)
     plt.clf()
 
