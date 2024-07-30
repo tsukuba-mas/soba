6db1f9874f026cdd18225abb6b06c6903879603c
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-07-30 15:02:08 +0900

    output log if OD is not possible

--- src/interactions/opinionDynamics.nim
+++ src/interactions/opinionDynamics.nim
@@ -8,9 +8,11 @@ import strformat
 
 proc opinionDynamics(agent: Agent, acceptablePosts: seq[Message], tick: int): Agent =
   let neighbors = acceptablePosts.mapIt(it.opinion)
-  if neighbors.len == 0:
-    return agent
-  let updatedOpinion = (1.0 - agent.mu) * agent.opinion + agent.mu * mean(neighbors)
+  let updatedOpinion = 
+    if neighbors.len > 0: 
+      (1.0 - agent.mu) * agent.opinion + agent.mu * mean(neighbors)
+    else:
+      agent.opinion
   verboseLogger(
     fmt"OD {tick} {agent.id} {agent.opinion} -> {updatedOpinion}",
     tick
