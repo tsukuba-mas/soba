3760a4f619aa36a430db7051c96f10933e418f1f
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-08-27 21:38:57 +0900

    interact all of the agents at the same time

--- src/interactions/chooseTargets.nim
+++ src/interactions/chooseTargets.nim
@@ -1,10 +1,6 @@
-import utils
 import sequtils
-import sets
-import algorithm
 import options
 import ../types
 
 proc chooseTargets*(agents: int): Option[seq[Id]] = 
-  # use takeN intentionally
-  (0..<agents).toSeq.map(toId).takeN(1)
+  (0..<agents).toSeq.map(toId).some()
