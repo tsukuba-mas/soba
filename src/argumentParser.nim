8bc8679c12f2d0950af27d323ede510ddf36d74d
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-09-13 15:13:22 +0200

    does not use argparse, just receive a toml file

--- src/argumentParser.nim
+++ src/argumentParser.nim
@@ -1,11 +1,9 @@
-import argparse
 import types
 import strutils
 import sequtils
-import algorithm
 import parsetoml
-import options
 import intbrg
+import os
 
 proc optionsFromToml(tomlPath: string): CommandLineArgs =
   let toml = parseFile(tomlPath)
@@ -32,71 +30,10 @@ proc optionsFromToml(tomlPath: string): CommandLineArgs =
     topic: toml["topic"].getStr().toFormula,
   )
 
-proc parseValue(path: string): seq[float] =
-  var f = open(path)
-  defer:
-    f.close()
-  f.readAll().split("\n").map(parseFloat)
-
 proc parseArguments*(): CommandLineArgs =
-  var p = newParser:
-    help("echo chamber simulator based on opinion dynamics and belief revision")
-    option("-s", "--seed", help="seed", default=some("42"))
-    option("-d", "--dir", help="directory to save files", default=some("out"))
-    option("-a", "--agents", help="the number of agents", default=some("100"))
-    option("-f", "--follow", help="the number of follows (i.e., edges)", default=some("400"))
-    option("-t", "--tick", help="the number of iterations", default=some("100"))
-    option("--fs", "--filter", help="filter strategy", default=some("all"))
-    option("--us", "--updating", help="updating strategy", default=some("independent"))
-    option("--rs", "--rewriting", help="rewriting strategy", default=some("none"))
-    option("--mu", help="parameter in opinion dynamics", default=some("0.5"))
-    option("--alpha", help="parameter in opinion formation", default=some("0.5"))
-    option("--up", "--unfollow", help="probability of unfollowing agents", default=some("0.5"))
-    option("--rp", "--repost", help="probability of reposting", default=some("0.5"))
-    option("--ap", "--activation", help="probability of activation", default=some("0.5"))
-    option("-v", "--values", help="path to cultural values")
-    option("--epsilon", help="threshold for opinions", default=some("0.01"))
-    option("--delta", help="threshold for beliefs", default=some("4"))
-    option("--topic", help="topic", default=some("11110000"))
-    option("-l", "--screen", help="set screen size", default=some("10"))
-    flag("--verbose", help="verbose mode")
-    option("--toml", help="input TOML format file", default=none(string))
-  
-  try:
-    let parsed = p.parse()
-    if parsed.toml.len > 0:
-      # if --toml is given
-      return optionsFromToml(parsed.toml)
-    else:
-      return CommandLineArgs(
-        seed: parsed.seed.parseInt,
-        dir: parsed.dir,
-        n: parsed.agents.parseInt,
-        follow: parsed.follow.parseInt,
-        tick: parsed.tick.parseInt,
-        filter: parseEnum[FilterStrategy](parsed.filter),
-        update: @[parseEnum[UpdatingStrategy](parsed.updating)],
-        rewriting: parseEnum[RewritingStrategy](parsed.rewriting),
-        verbose: parsed.verbose,
-        mu: parsed.mu.parseFloat,
-        alpha: parsed.alpha.parseFloat,
-        unfollowProb: parsed.unfollow.parseFloat,
-        repostProb: parsed.repost.parseFloat,
-        activationProb: parsed.activation.parseFloat,
-        values: 
-          if parsed.values.len == 0: (0..7).toSeq.reversed.mapIt(it.toFloat / 7.0) 
-          else: parsed.values.parseValue,
-        epsilon: parsed.epsilon.parseFloat,
-        delta: parsed.delta.parseInt,
-        atomicProps: 3,
-        screenSize: parsed.screen.parseInt,
-        topic: parsed.topic.toFormula,
-      )
-  except ShortCircuit as err:
-    # show help message
-    echo err.help
-    echo "fs: ", (FilterStrategy.low..FilterStrategy.high).toSeq.join(", ")
-    echo "us: ", (UpdatingStrategy.low..UpdatingStrategy.high).toSeq.join(", ")
-    echo "rs: ", (RewritingStrategy.low..RewritingStrategy.high).toSeq.join(", ")
-    echo "NB: use EITHER --toml OR other parameters. If --toml is used, other parameters will be ignored."
-    quit(0)
+  let params = commandLineParams()
+  assert(params.len > 0, "Empty parameter")
+  let toml = params[0]
+  if not fileExists(toml):
+    assert(false, toml & " does not exist")
+  optionsFromToml(toml)
