e0566ae1fa45ebc41432a846ce99acbdbbc044aa
Hiro KATAOKA <kataoka@mas.cs.tsukuba.ac.jp>
2024-09-24 16:32:20 +0200

    use more suitable variable names

--- src/interactions/interactions.nim
+++ src/interactions/interactions.nim
@@ -7,24 +7,24 @@ import relaxDissonance
 import intbrg
 import tables
 
-proc performInteractions(agent: Agent, evaluatedPosts: EvaluatedTimeline, topic: Formulae, tick: int): Agent =
+proc performInteractions(agent: Agent, messages: seq[Message], topic: Formulae, tick: int): Agent =
   var newAgent = agent
   for strategy in agent.updatingStrategy:
     newAgent = case strategy
     of UpdatingStrategy.od:
-      newAgent.opinionDynamics(evaluatedPosts.acceptables, tick)
+      newAgent.opinionDynamics(messages, tick)
     of UpdatingStrategy.br:
-      newAgent.beliefRevisionGames(evaluatedPosts.acceptables, tick)
+      newAgent.beliefRevisionGames(messages, tick)
     of UpdatingStrategy.ba:
       newAgent.beliefAlignment(topic, tick)
     of UpdatingStrategy.`of`:
       newAgent.opinionFormation(topic, tick)
   return newAgent
 
-proc performInteractions*(simulator: Simulator, evaluatedPosts: Table[Id, EvaluatedTimeline], tick: int): Simulator =
+proc performInteractions*(simulator: Simulator, id2messages: Table[Id, seq[Message]], tick: int): Simulator =
   let newAgents = simulator.agents.mapIt(
-    if evaluatedPosts.hasKey(it.id):
-      performInteractions(it, evaluatedPosts[it.id], simulator.topic, tick)
+    if id2messages.hasKey(it.id):
+      performInteractions(it, id2messages[it.id], simulator.topic, tick)
     else:
       it
   )
